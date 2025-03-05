4f9c2648747238140686d94fa9127b7e1a48ba83
Oluwadamilola Oyaluade <167680028+Olu199@users.noreply.github.com>
2025-03-05 15:40:07 -0600

    Add admin auth

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor, CKEditorField
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_bootstrap import Bootstrap5
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 
 
@@ -13,9 +14,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///articles.db'
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", os.urandom(24).hex())
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 bootstrap = Bootstrap5(app)
 ckeditor = CKEditor(app)
 db = SQLAlchemy(app)
+
 
 class ArticleDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +27,8 @@ class ArticleDB(db.Model):
     thumbnail_image = db.Column(db.String(200), nullable=False)
     banner_image = db.Column(db.String(200), nullable=False)
     subtitle = db.Column(db.String(400), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     body = db.Column(db.Text, nullable=False)
 
 class ArticleForm(FlaskForm):
@@ -30,44 +36,84 @@ class ArticleForm(FlaskForm):
     thumbnail_image = StringField("Thumbnail Image", validators=[DataRequired(), URL()])
     banner_image = StringField("Banner Image", validators=[DataRequired(), URL()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
+    # date = StringField("Date", validators=[DataRequired()])
+    # author_id = StringField("Author ID", validators=[DataRequired()])
     body = CKEditorField("Body", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    user_name = db.Column(db.String(100), unique=True)    
+    user_articles = db.relationship('ArticleDB', backref='author', lazy=True)
+
+class UserForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    user_name = StringField("User Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class ContactForm(FlaskForm):
+    first_name = StringField("First Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    message = CKEditorField("Message", validators=[DataRequired()])
 
 # Create Database
 with app.app_context():
     db.create_all()
 
-    if ArticleDB.query.count() == 0:
-        sample_articles = [
-            ArticleDB(title="AWS Cloud Best Practices",  thumbnail_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", banner_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", 
-                    subtitle="Guide to AWS best practices for scalability and security.", body="CKEBODYGuide to AWS best practices for scalability and security."
-                    ),
 
-            ArticleDB(title="Machine Learning Basics",  thumbnail_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", banner_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", 
-                    subtitle="Introduction to supervised and unsupervised learning.", body="CKEBODYGuide to AWS best practices for scalability and security."
-                    ),
 
-            ArticleDB(title="John 3:16 Reflection",  thumbnail_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", banner_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", 
-                    subtitle="A personal reflection on faith and the love of God.", body="CKEBODYGuide to AWS best practices for scalability and security."
-                    ),
 
-            ArticleDB(title="Proverbs on Wisdom",  thumbnail_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", banner_image="https://th.bing.com/th/id/OIP.axQ6oXfHg3Hw6f2lWc7h5QHaEK?rs=1&pid=ImgDetMain", 
-                    subtitle="Understanding wisdom in biblical teachings.", body="CKEBODYGuide to AWS best practices for scalability and security."
-                    )
-        ]
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-        db.session.add_all(sample_articles)
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    user_form = UserForm()
+    if user_form.validate_on_submit():
+        new_user = User(
+            email=user_form.email.data,
+            password=user_form.password.data,
+            first_name=user_form.first_name.data,
+            last_name=user_form.last_name.data,
+            user_name=user_form.user_name.data
+        )
+        db.session.add(new_user)
         db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('index'))
+    return render_template('register.html', user_form=user_form)
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = db.session.query(User).filter_by(email=email, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
 
-
+@app.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route("/")
 def index():
     articles = db.session.query(ArticleDB).all()
     articles = [article for article in articles]
-    return render_template("index.html", articles=articles)
+    return render_template("index.html", articles=articles, is_authenticated=current_user.is_authenticated)
 
 
 @app.route('/article/<int:article_id>/', methods=['GET'])
@@ -86,12 +132,14 @@ def new_article():
             thumbnail_image=article_form.thumbnail_image.data,
             banner_image=article_form.banner_image.data,
             subtitle=article_form.subtitle.data,
-            body=article_form.body.data
+            # body=article_form.body.data,
+            # date=article_form.date.data,
+            author_id=current_user.id
         )
         db.session.add(new_article)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('editor.html', article_form=article_form)
+    return render_template('editor.html', article_form=article_form, is_authenticated=current_user.is_authenticated)
 
 
 @app.route('/edit_article/<int:article_id>/', methods=['GET', 'POST'])
@@ -102,7 +150,9 @@ def edit_article(article_id):
         thumbnail_image = existing_article.thumbnail_image,
         banner_image = existing_article.banner_image,
         subtitle = existing_article.subtitle,
-        body = existing_article.body
+        body = existing_article.body,
+        date = existing_article.date,
+        author_id = existing_article.author_id
       )
     
     if article_form.validate_on_submit():
