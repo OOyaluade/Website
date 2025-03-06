9112c8889f230cc065205fd1305b1fe8d8b8250b
Oluwadamilola Oyaluade <167680028+Olu199@users.noreply.github.com>
2025-03-06 11:10:32 -0600

    add

--- main.py
+++ main.py
@@ -262,4 +262,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='127.0.0.1', help='The host address to bind to')
     args = parser.parse_args()
-    app.run(host=args.host, debug=True)
+    app.run(host=args.host, debug=False)
