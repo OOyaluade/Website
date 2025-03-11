06b2ab82e8c5e644fa644121a8bd741ca3a58d8e
Oluwadamilola Oyaluade <oyaluadedamilola@damilstudio.com>
2025-03-11 14:46:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -263,4 +263,6 @@ if __name__ == '__main__':
     # parser.add_argument('--host', default='127.0.0.1', help='The host address to bind to')
     # args = parser.parse_args()
     # app.run(host=args.host, debug=False)
-        app.run(debug=False)
+
+    app.run(host="0.0.0.0", port=5000)
+
